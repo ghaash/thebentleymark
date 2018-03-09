1afe646c7aeb80dcafa61e4fa3c7b1f4cbf49506
ghaash <daniel.silverman@me.com>
2018-03-09 11:15:46 -0800

    added background color toggle

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 alert("welcome to the bentley mark!");
+
+
+//makes cell yellow and timeouts 100ms
+function toggleBackground()
+{
+
+    redAndBlue = document.getElementById("colorToggle");
+    redAndBlue.style.backgroundColor = "blue";
+
+    // setTimeout(function()
+    // {
+    //     alert("cell is now highlighted");
+    // }, 100);
+
+    // below is logic to toggle which we can't do :(
+    if(redAndBlue.style.backgroundColor == "blue")
+    {
+        redAndBlue.style.backgroundColor = "red"
+    }
+    else
+    {
+        redAndBlue.style.backgroundColor = "blue";
+    }
+}
+
+//event listener for markCell
+document.getElementById("toggleBackground").addEventListener("click", toggleBackground);
